41b98f1868582ba71601d0a43fbc9fd4c2ff98cb
tetsuyainfra <tetsuyainfra@gmail.com>
2023-08-27 23:02:51 +0900

    can linux build

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
-
 # structure
+
 - server
 - frontend(vite)
 
-# setup server 
+# setup server
 
 ```
 cargo add axum --git=https://github.com/tokio-rs/axum --branch=main
 cargo add tracing tracing-subscriber
 ```
+
 cargo add tokio --features full
 cargo add hyper@1.0.0-rc.4 --features=full
 cargo add tower
 cargo add tower-http --features=cors,fs,trace
 cargo add tracing
 cargo add tracing tracing-subscriber --features=env-filter
+
+# build
+
+```
+CROSS_CONTAINER_ENGINE=podman cross run --target x86_64-pc-windows-gnu
+```
+
+## 覚書
+
+- build.rs が毎回走るのでこのままだと npm run build が都度実行される
+  境変変数で ON/OFF するか、なにかファイルを見て実行制御したいところ
+
+- npm がないよー
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,18 @@ use std::net::SocketAddr;
 
 use axum::{
     body::Body,
-    extract::{ws::WebSocketUpgrade, Request, State},
-    http::{request, HeaderValue},
+    extract::{Request, State},
+    http::HeaderValue,
     response::{Html, IntoResponse, Response},
     routing::get,
     Json, Router,
 };
 
 use axum::extract::connect_info::ConnectInfo;
-use axum::extract::ws::CloseFrame;
 
-use axum_extra::{headers, TypedHeader};
-use hyper::{Method, StatusCode};
+use hyper::{header, Method, StatusCode};
 use reqwest::Client;
+use rust_embed::RustEmbed;
 use serde_json::json;
 use tokio::net::TcpListener;
 use tower_http::{
@@ -24,21 +23,36 @@ use tower_http::{
 use tracing::info;
 use tracing_subscriber::EnvFilter;
 
+#[derive(Debug, Clone)]
+enum UiProxyMode {
+    Embed,
+    Proxy,
+}
+
 #[tokio::main]
 async fn main() {
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", format!("debug,hyper=info"))
     }
+    let proxy_mode = match std::env::var("UI_PROXY_MODE") {
+        Ok(val) => {
+            if val.to_uppercase() == "PROXY" {
+                UiProxyMode::Proxy
+            } else {
+                UiProxyMode::Embed
+            }
+        }
+        Err(_) => UiProxyMode::Embed,
+    };
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let client = Client::new();
-
     let app = Router::new()
         .route("/", get(root_handler))
         .route("/api/1", get(api_handler))
-        .route("/ui", get(ui_proxy))
+        .route("/ui", get(ui_proxy)) //  http://foobar/ui に対応させる時
+        .route("/ui/", get(ui_proxy)) //  http://foobar/ui/ に対応させる時
         .route("/ui/*wild", get(ui_proxy))
         .layer(
             CorsLayer::new()
@@ -51,7 +65,7 @@ async fn main() {
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
         )
-        .with_state(client)
+        .with_state(proxy_mode)
         .into_make_service_with_connect_info::<SocketAddr>();
 
     let sock_addr = "127.0.0.1:3000".parse::<SocketAddr>().unwrap();
@@ -62,37 +76,96 @@ async fn main() {
     axum::serve(tcp_listener, app.clone()).await.unwrap();
 }
 
-async fn root_handler(ConnectInfo(remote_addr): ConnectInfo<SocketAddr>) -> impl IntoResponse {
+async fn root_handler(
+    State(mode): State<UiProxyMode>,
+    ConnectInfo(remote_addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
     Html(format!(
         r##"root from {remote_addr}<br />
-            <a href="/ui">ui</a>"##
+            MODE: {mode:?}
+            <a href="/ui">ui</a><br />
+        "##
     ))
 }
 async fn api_handler(_req: Request) -> impl IntoResponse {
     Json(json!({ "data": 42 }))
 }
 
-async fn ui_proxy(State(client): State<Client>, req: Request) -> impl IntoResponse {
-    // include_dir!("")
-    let uri = req.uri();
-    let url = format!("http://127.0.0.1:5173{uri}");
-    info!(?url);
+// async fn ui_proxy(State(proxy_mode): State<UiProxyMode>, req: Request) -> impl IntoResponse {
+//     match proxy_mode {
+//         UiProxyMode::Embed => ui_proxy_embed(req).await.into_response(),
+//         UiProxyMode::Proxy => ui_proxy_proxy(req).await.into_response(),
+//     }
+// }
 
-    let reqwest_response = match client.get(url).send().await {
-        Ok(res) => res,
-        Err(err) => {
-            tracing::error!(%err, "request failed");
-            return StatusCode::BAD_GATEWAY.into_response();
+async fn ui_proxy(State(proxy_mode): State<UiProxyMode>, req: Request) -> impl IntoResponse {
+    // async fn ui_proxy_proxy(req: Request) -> impl IntoResponse {
+    let mut path = req.uri().path().trim_start_matches('/').to_string();
+    if path == "ui" {
+        // "ui" -> ""
+        path = path.replace("ui", "");
+    } else if path.starts_with("ui/") {
+        // "ui/" -> ""
+        // "ui/index.html" -> "index.html"
+        path = path.replace("ui/", "");
+    }
+
+    match proxy_mode {
+        UiProxyMode::Embed => StaticFile(path).into_response(),
+        UiProxyMode::Proxy => {
+            // include_dir!("")
+            let url = format!("http://127.0.0.1:5173/ui/{path}");
+            info!(?url);
+
+            let client = Client::new();
+            let reqwest_response = match client.get(url).send().await {
+                Ok(res) => res,
+                Err(err) => {
+                    tracing::error!(%err, "request failed");
+                    return StatusCode::BAD_GATEWAY.into_response();
+                }
+            };
+
+            let mut response_builder = Response::builder().status(reqwest_response.status());
+
+            *response_builder.headers_mut().unwrap() = reqwest_response.headers().clone();
+
+            response_builder
+                // .header("Permissions-Policy", "geolocation=*") // 必要なさそう
+                .body(Body::from_stream(reqwest_response.bytes_stream()))
+                // Same goes for this unwrap
+                .unwrap()
+                .into_response()
         }
-    };
+    }
+}
 
-    let mut response_builder = Response::builder().status(reqwest_response.status());
+#[derive(RustEmbed)]
+#[folder = "client/dist/"]
+struct Asset;
 
-    *response_builder.headers_mut().unwrap() = reqwest_response.headers().clone();
+pub struct StaticFile<T>(pub T);
 
-    response_builder
-        // .header("Permissions-Policy", "geolocation=*") // 必要なさそう
-        .body(Body::from_stream(reqwest_response.bytes_stream()))
-        // Same goes for this unwrap
-        .unwrap()
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = String::from(self.0.into());
+        info!("StaticFile request -> '{path}'");
+
+        let req_path = match path.as_str() {
+            "" => "index.html",
+            _ => &path,
+        };
+
+        match Asset::get(req_path) {
+            Some(content) => {
+                let mime = mime_guess::from_path(req_path).first_or_octet_stream();
+                info!("StaticFile mime -> {mime:?}");
+                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+            }
+            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        }
+    }
 }
